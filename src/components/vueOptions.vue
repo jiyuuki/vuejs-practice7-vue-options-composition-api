9b459c6742aa6ff75959120544c568bc94266d4c
EL HAJRAOUI Salma <salma.elhajraoui@docaposte.fr>
2021-07-08 15:37:16 +0200

    addItem on list

--- src/components/vueOptions.vue
+++ src/components/vueOptions.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-        <h5 class="card-title">Vue options - you have {{ itemsCount }} item added</h5>
+        <h5 class="card-title">Vue options - you have {{ getItemsCount }} item added</h5>
         <div class="input-group mb-3">
             <input 
                 type="text" 
@@ -9,15 +9,20 @@
                 ria-label="item" 
                 aria-describedby="basic-addon2"
                 @keyup.enter="addItem"
+                v-model="newItem"
             >
             <div class="input-group-append">
                 <button class="btn btn-outline-secondary" type="button" @click="addItem">Add</button>
             </div>
         </div>
         <ul class="list-group">
-            <li class="list-group-item d-flex justify-content-between align-items-center">
+            <li 
+                class="list-group-item d-flex justify-content-between align-items-center"
+                v-for="(item, index) in items"
+                :key="index"
+                >
                 <span class="badge badge-primary badge-pill">{{ itemCount }}</span>
-                Cras justo odio
+                {{ item.value }}
                 <span class="badge badge-primary badge-pill" @click="deleteItem">X</span>
             </li>
         </ul>
@@ -26,17 +31,42 @@
 
 <script>
 export default {
-    name: 'vueOptions',
+    name: "vueOptions",
     data () {
         return {
             itemsCount: 0,
             itemCount: 1,
+            newItem: "",
+            items: [
+                {
+                    value: "One"
+                },
+                {
+                    value: "Two"
+                },
+                {
+                    value: "bla bla"
+                }
+            ]
 
+        }
+    },
+    computed: {
+        getItemsCount () {
+            return this.items.length;
         }
     },
     methods: {
         addItem () {
             console.log('addItem')
+            this.items.push({value: this.newItem})
+            this.newItem = ''; 
+            TODO:
+            // up itemCount if item is already existe
+            // addItem if newItem is not exsted
+            console.log(this.items)
+            console.log(this.newItem)
+            console.log(this.itemsCount)
         },
         deleteItem () {
             console.log('deleteItem')
